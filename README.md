e14c6f1c9a8b057686007ae4aff0fd97d05edf4c
Alexander Curtis <alexcu@tutanota.com>
2025-07-04 09:35:00 +0100

    tidied up features, bumped update

--- README.md
+++ README.md
@@ -1,20 +1,19 @@
 # compile_time_ls_colours
 
-i will update docs soon, just check tests
+Temporary crate for file extension colour lookups using LS_COLORS format.
 
-Temporary crate for file extension color lookups using LS_COLORS format.
-
-The main idea is a compile time hash map (we can't use std)
+The main idea is a compile time hash map (we can't use std hashmap due to complicated reasons)
 
 It provides colour coding for file types in terminal applications. Keys are byte slices representing file extensions. Values are byte slices representing ///ANSI escape sequences. Generated at build time from the LS_COLORS environment variable.
 
 ## Quick Example
 
 ```rust
-use compile_time_ls_colours::
+use compile_time_ls_colours::colour_path_or_reset;
 
 // Get color code for .rs files, fallback to reset code if not found
-let color = colour_hashmap(b"rs", b"\x1b[0m");
+let extension:&[u8]=b"rs";
+let color = colour_path_or_reset(extension)
 println!("\x1b[{}m*.rs\x1b[0m", String::from_utf8_lossy(color));
 ```
 
@@ -25,18 +24,40 @@ println!("\x1b[{}m*.rs\x1b[0m", String::from_utf8_lossy(color));
 pub static LS_COLOURS_HASHMAP: Map<&'static [u8], &'static [u8]>
 
 
-/// I've added some convenience functions to use below :)
-
-////// Returns the color code for a given file extension.
-/// If the extension is not found in the LS_COLORS_MAP, it returns the provided alternative
-pub fn colour_hashmap<'a>(ext:&'a[u8],or_alternative:&'a[u8])->&'a[u8]{
-    LS_COLOURS_HASHMAP.get(ext).map(|v| &**v).unwrap_or_else(||or_alternative)
+pub fn colour_path_or_alternative<'a>(extension:&'a [u8],or_alternative:&'a [u8])->&'a [u8]{
+    LS_COLOURS_HASHMAP.get(extension).map(|v| &**v).unwrap_or_else(||or_alternative)
 }
 
 
-// Returns the color code for if it exists
-pub fn colour_map<'a>(ext:&'a[u8])->Option<&'a[u8]>{
-    LS_COLOURS_HASHMAP.get(ext).map(|v| &**v)
+/// Returns the colour code for a given file extension if it exists in the color map.
+/// Returns `None` if not found.
+pub fn colour_path(extension:&'static [u8])->Option<&'static[u8]>{
+    LS_COLOURS_HASHMAP.get(extension).map(|v| &**v)
+}
+
+
+/// Like `colour_path_or_alternative`, but defaults to `NO_COLOR` if extension is not recognized.
+/// This is useful for cases where you want to ensure a reset color code is used
+/// when the file type is not recognized.
+pub fn colour_path_or_reset<'a>(extension: &'a  [u8]) -> &'a [u8] {
+    LS_COLOURS_HASHMAP.get(extension).map(|v| &**v).unwrap_or_else(|| NO_COLOUR)
+}
+
+
+
+#[macro_export]
+//DO NOT USE RAW LITERALS AKA b"py" IN THIS!
+macro_rules! file_type_colour {
+    
+    (symlink) => { // if it's a symlink, use the default symlink colour
+        $crate::colour_path_or_alternative(b"symlink", $crate::DEFAULT_SYMLINK_COLOUR)
+    };
+    (directory) => { // if it's a directory, use the default directory colour
+        $crate::colour_path_or_alternative(b"directory", $crate::DEFAULT_DIR_COLOUR)
+    };
+    ($other:ident) => { // for any other file type, use the colour from the LS_COLORS map or NO_COLOUR
+        $crate::colour_path_or_alternative($other, $crate::NO_COLOUR)
+    };
 }
 
 
--- src/lib.rs
+++ src/lib.rs
@@ -4,22 +4,22 @@ include!(concat!(env!("OUT_DIR"), "/ls_colors.rs"));
 mod test;
 
 
-pub const NO_COLOUR: &[u8] = b"\x1b[0m"; // Reset color code
-// Default color code for symbolic links.
-pub const DEFAULT_SYMLINK_COLOR: &[u8] = b"\x1b[38;2;230;150;60m";
+pub const NO_COLOUR: &[u8] = b"\x1b[0m"; // Reset colour code
+// Default colour code for symbolic links.
+pub const DEFAULT_SYMLINK_COLOUR: &[u8] = b"\x1b[38;2;230;150;60m";
 
 // Default colour code for directories.
 pub const DEFAULT_DIR_COLOUR: &[u8] = b"\x1b[38;2;30;144;255m";
 
 /// Returns the colour code for a given file extension.
-/// Falls back to `or_alternative` if the extension is not found in the color map.
+/// Falls back to `or_alternative` if the extension is not found in the colour map.
 #[inline]
 pub fn colour_path_or_alternative<'a>(extension:&'a [u8],or_alternative:&'a [u8])->&'a [u8]{
     LS_COLOURS_HASHMAP.get(extension).map(|v| &**v).unwrap_or_else(||or_alternative)
 }
 
 
-/// Returns the colour code for a given file extension if it exists in the color map.
+/// Returns the colour code for a given file extension if it exists in the colour map.
 /// Returns `None` if not found.
 #[inline]
 pub fn colour_path(extension:&'static [u8])->Option<&'static[u8]>{
@@ -28,18 +28,18 @@ pub fn colour_path(extension:&'static [u8])->Option<&'static[u8]>{
 
 
 /// Like `colour_path_or_alternative`, but defaults to `NO_COLOR` if extension is not recognized.
-/// This is useful for cases where you want to ensure a reset color code is used
+/// This is useful for cases where you want to ensure a reset colour code is used
 /// when the file type is not recognized.
 #[inline]
 pub fn colour_path_or_reset<'a>(extension: &'a  [u8]) -> &'a [u8] {
     LS_COLOURS_HASHMAP.get(extension).map(|v| &**v).unwrap_or_else(|| NO_COLOUR)
 }
 
-/// Macro to generate a color code for a file type based on its name/type.
+/// Macro to generate a colour code for a file type based on its name/type.
 ///
-/// It uses the `colour_path_or_alternative` function to retrieve a color from
+/// It uses the `colour_path_or_alternative` function to retrieve a colour from
 /// the internal LS_COLORS map. If the file type is not recognized, it falls
-/// back to a sensible default such as `NO_COLOR`, `DEFAULT_SYMLINK_COLOR`, or `DEFAULT_DIR_COLOR`.
+/// back to a sensible default such as `NO_COLOR`, `DEFAULT_SYMLINK_COLOUR`, or `DEFAULT_DIR_COLOUR`.
 ///
 /// Please note, you cannot use raw string literals arguments with this macro.
 /// 
@@ -48,17 +48,17 @@ pub fn colour_path_or_reset<'a>(extension: &'a  [u8]) -> &'a [u8] {
 /// ```rust
 /// use compile_time_ls_colours::{file_type_colour, NO_COLOUR};
 ///
-/// // Get color for a symlink
-/// let symlink_color: &[u8] = file_type_colour!(symlink);
+/// // Get colour for a symlink
+/// let symlink_colour: &[u8] = file_type_colour!(symlink);
 ///
-/// // Get color for a directory
-/// let dir_color: &[u8] = file_type_colour!(directory);
+/// // Get colour for a directory
+/// let dir_colour: &[u8] = file_type_colour!(directory);
 /// let ext_rs= b"rs";
-/// // Get color for a known extension (e.g., b"rs")
-/// let rs_color: &[u8] = file_type_colour!(ext_rs);
-/// // Get fallback color if extension is not in the map
+/// // Get colour for a known extension (e.g., b"rs")
+/// let rs_colour: &[u8] = file_type_colour!(ext_rs);
+/// // Get fallback colour if extension is not in the map
 /// let ext = b"txt";
-/// let unknown_color: &[u8] = file_type_colour!(ext); // defaults to NO_COLOUR
+/// let unknown_colour: &[u8] = file_type_colour!(ext); // defaults to NO_COLOUR
 /// ```
 /// 
 /// 
@@ -66,7 +66,7 @@ pub fn colour_path_or_reset<'a>(extension: &'a  [u8]) -> &'a [u8] {
 macro_rules! file_type_colour {
     
     (symlink) => { // if it's a symlink, use the default symlink colour
-        $crate::colour_path_or_alternative(b"symlink", $crate::DEFAULT_SYMLINK_COLOR)
+        $crate::colour_path_or_alternative(b"symlink", $crate::DEFAULT_SYMLINK_COLOUR)
     };
     (directory) => { // if it's a directory, use the default directory colour
         $crate::colour_path_or_alternative(b"directory", $crate::DEFAULT_DIR_COLOUR)
